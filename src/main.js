56a1c55e36bd13442bc5c970595497ca3d20f26b
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-06-14 11:33:29 +0200

    Maj Version style

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import { loadFonts } from './plugins/webfontloader'
 
 import LottieAnimation from "lottie-web-vue";
-
+import { VueEditor } from "vue3-editor";
 
 import vuetify from './plugins/vuetify'
 import router from './router'
@@ -70,4 +70,5 @@ loadFonts()
   .use(router)
   .use(store)
   .use(LottieAnimation)
+  .use(VueEditor)
   .mount('#app')
